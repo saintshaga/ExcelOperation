4dce0ee8913178e364afc49ba03ab50ad331996c
huang_f <huang_f@worksap.co.jp>
2013-05-29 17:46:08 +0800

    init of maven project

---
+++ src/main/java/cn/saintshaga/ExcelOperation/App.java
@@ -0,0 +1,13 @@
+package cn.saintshaga.ExcelOperation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
